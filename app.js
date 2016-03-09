f6428d27b3bad4e8fdc77e39c6223532a4d4f87b
andrewaliu <liu.anray@gmail.com>
2016-03-09 11:06:00 -0500

    /compare is routed to a different page

--- app.js
+++ app.js
@@ -3,27 +3,30 @@ var fs = require('fs');
 var request = require('request');
 
 var server = http.createServer(function(req, res) {
+  if (req.url === '/') {
+    request('http://api.population.io/1.0/countries', function (error, response, body) {
+      if (error || response.statusCode != 200) {
+        console.error(error.message);
+        res.end();
+      }
+      var countries = JSON.parse(body).countries;
 
-  request('http://api.population.io/1.0/countries', function (error, response, body) {
-    if (error || response.statusCode != 200) {
-      console.error(error.message);
+      var options = '';
+      for (var i = 0; i < countries.length; i++) {
+        var country = countries[i];
+        options += '<option>' + country + '</option>\n';
+      }
+
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      var html = fs.readFileSync('main.html', 'utf-8');
+      html = html.replace('{{options1}}', options);
+      html = html.replace('{{options2}}', options);
+      res.write(html);
+
       res.end();
-    }
-    var countries = JSON.parse(body).countries;
-
-    var options = '';
-    for (var i = 0; i < countries.length; i++) {
-      var country = countries[i];
-      options += '<option>' + country + '</option>\n';
-    }
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var html = fs.readFileSync('main.html', 'utf-8');
-    html = html.replace('{{options1}}', options);
-    html = html.replace('{{options2}}', options);
-    res.write(html);
-
-    res.end();
-  });
+    });
+  } else {
+    res.end('Compare');
+  }
 
 }).listen(8080);
