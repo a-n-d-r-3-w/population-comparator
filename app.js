b9b06fef5e7564a80b6a5eaf8c6431ba677a5d92
andrewaliu <liu.anray@gmail.com>
2016-03-09 12:21:49 -0500

    get data for both countries

--- app.js
+++ app.js
@@ -6,33 +6,56 @@ var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
   if (req.url === '/') {
-    request('http://api.population.io/1.0/countries', function (error, response, body) {
-      if (error || response.statusCode != 200) {
-        console.error(error.message);
+    request('http://api.population.io/1.0/countries',
+      (error, response, body) => {
+        if (error || response.statusCode != 200) {
+          console.error(error.message);
+          res.end();
+        }
+        var countries = JSON.parse(body).countries;
+
+        var options = '';
+        for (var i = 0; i < countries.length; i++) {
+          var country = countries[i];
+          options += '<option>' + country + '</option>\n';
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var html = fs.readFileSync('main.html', 'utf-8');
+        html = html.replace('{{options1}}', options);
+        html = html.replace('{{options2}}', options);
+        res.write(html);
+
         res.end();
       }
-      var countries = JSON.parse(body).countries;
-
-      var options = '';
-      for (var i = 0; i < countries.length; i++) {
-        var country = countries[i];
-        options += '<option>' + country + '</option>\n';
-      }
-
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      var html = fs.readFileSync('main.html', 'utf-8');
-      html = html.replace('{{options1}}', options);
-      html = html.replace('{{options2}}', options);
-      res.write(html);
-
-      res.end();
-    });
+    );
   } else {
     var parseQueryString = true;
     var query = url.parse(req.url, parseQueryString).query;
     var country1 = query.country1;
     var country2 = query.country2;
-    res.end('Compare ' + country1 + ' with ' + country2 + '.');
+    request('http://api.population.io:80/1.0/population/1980/' + country1 + '/',
+      (error, response, body) => {
+        if (error || response.statusCode != 200) {
+          console.error(error.message);
+          res.end();
+        }
+        var country1Data = JSON.parse(body);
+        console.dir(country1Data);
+
+        request('http://api.population.io:80/1.0/population/1980/' + country2 + '/',
+          (error, response, body) => {
+            if (error || response.statusCode != 200) {
+              console.error(error.message);
+              res.end();
+            }
+            var country2Data = JSON.parse(body);
+            console.dir(country2Data);
+            res.end('Compare ' + country1 + ' with ' + country2 + '.');
+          }
+        );
+      }
+    );
   }
 
 }).listen(8080);
